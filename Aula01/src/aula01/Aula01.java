143f1c28df53a4b0c201c642b010c0568cc10deb
Kevin-Souza <78280970+Kevin-Souza@users.noreply.github.com>
2021-04-18 18:33:42 -0300

    Initial commit

---
+++ Aula01/src/aula01/Aula01.java
@@ -0,0 +1,61 @@
+
+package aula01;
+import java.util.Scanner;
+public class Aula01 {
+    public static void main(String[] args) {
+        //System.out.println("Aluno     NOTA");
+        //System.out.println("==========        =====");
+       // System.out.println("ALINE     9.0");
+       // System.out.println("MÁRIO     10.0");
+       // System.out.println("SÉRGIO    4.5");
+       // System.out.println("HIRLEY    7.0");
+       
+       // declarar a variavel
+       //int idade; // S.O escolha endereços de RAM que caiba um inteiro (4 bytes) e chame de idade
+       // iniciar a variável
+       //idade = 0;
+       
+       //equivalente a declarar e inicializar
+       //int idade2 =0;
+       
+       //byte tipoByte = 127;
+       //short tipoShort = 32767;
+       //char tipoChar = 'C';
+       //float tipoFloat = 2.6f;
+       //double tipoDouble = 3.59;
+       //int tipoInt = 2147483647;
+       //long tipoLong = 9223372036854775807L;
+       //boolean tipoBoolean = true;
+       //System.out.println("Valor do tipoByte = " + tipoByte);
+       //System.out.println("Valor do tipoShort = " + tipoShort);
+       //System.out.println("Valor do tipoChar = " + tipoChar);
+       //System.out.println("Valor do tipoFloat = " + tipoFloat);
+       //System.out.println("Valor do tipoDouble = " + tipoDouble);
+       //System.out.println("Valor do tipoInt = " + tipoInt);
+       //System.out.println("Valor do tipoLong = " + tipoLong);
+       //System.out.println("Valor do tipoBoolean = " + tipoBoolean);
+       
+       //int Nro1,Nro2, Total;
+       //Nro1 = 10;
+       //Nro2 = 20;
+       //Total = Nro1 + Nro2;
+       //System.out.println("Valor Total:" + Total + "\n\n");
+       
+       
+       //Scanner O_leitor = new Scanner(System.in);
+       //int Nro1,Nro2, Total;
+       // System.out.println("Digite o número 1: ");
+       // Nro1 = O_leitor.nextInt();
+       // System.out.println("Digite o número 2:");
+       // Nro2 = O_leitor.nextInt();
+       // Total = Nro1 + Nro2;
+       // System.out.println("Valor total: " + Total + "\n\n");
+       
+       
+        
+       
+        
+        
+    }
+    
+}
---
+++ Exer01/src/exer01/Exer01.java
@@ -0,0 +1,167 @@
+package exer01;
+import java.util.Scanner;
+public class Exer01 {
+    public static void main(String[] args) {
+//      //exercício 1
+//      Scanner numero = new Scanner(System.in);
+//      double N1,N2,calc;
+//        System.out.println("Digite o primeiro número: ");
+//        //input.useLocale(Locale.ENGLISH);
+//        N1 = numero.nextDouble();
+//        
+//        System.out.println("Digite o segundo número: ");
+//        //input.useLocale(Locale.ENGLISH);
+//        N2 = numero.nextDouble();
+//        
+//        calc = (N1 * 0.4) + (N2*0.6);
+//        // calc = (N1 * 0.4f) + (N2*0.6f);   //para utilizar float, colocando cache somente 0.4 e 0.6 um numero.
+//        System.out.println("Valor da média é:" + calc);
+        
+        
+        
+        //exercício 2
+//        Scanner numero = new Scanner(System.in);
+//        float km,lit,cons;
+//        
+//        System.out.println("Digite os kilometros percorridos: ");
+//        km = numero.nextFloat();
+//        System.out.println("Digite o total gasto(L): ");
+//        lit = numero.nextFloat();
+//        
+//        cons = km/lit;
+//        System.out.println("O consumo de combustivel foi de: "+cons+"km por litro.");
+        
+//        //exercício 3
+//        Scanner numero = new Scanner(System.in);
+//        float peso,alt,imc;
+//        System.out.println("Digite seu peso: ");
+//        peso = numero.nextFloat();
+//        System.out.println("Digite sua altura: ");
+//        alt = numero.nextFloat();
+//        imc = peso/(alt*alt);
+//        System.out.println("Indice de massa corporal: "+imc);
+        
+        //exercício 4
+//        Scanner numero = new Scanner(System.in);
+//        int hora,dia,total;
+//        System.out.println("Digite a quantidade de horas trabalhadas por dia: ");
+//        hora = numero.nextInt();
+//        System.out.println("Digite a quantidade de dias: ");
+//        dia = numero.nextInt();
+//        total = dia*hora;
+//        System.out.println("O total de horas trabalhadas foi de: "+total+"horas");
+
+        //exercício 5
+//        Scanner salario = new Scanner(System.in);
+//        float sal,reajust;
+//        System.out.println("Digite o salário atual: ");
+//        sal = salario.nextFloat();
+//        reajust = sal+(sal*0.25f);
+//        System.out.println("O salário atual é de: "+reajust);
+
+        //exercício 6
+//        Scanner numero = new Scanner(System.in);
+//        int num,ant,suc;
+//        System.out.println("Digite um numero: ");
+//        num = numero.nextInt();
+//        ant = num - 1;
+//        suc = num + 1;
+//        System.out.println("O antecessor do numero"+num+" é: "+ant+" ,e sucessor"+ "é: "+suc);
+        
+        //exercício 7
+//        Scanner metro = new Scanner(System.in);
+//        float met,cm,mm;
+//        System.out.println("Digite a quantidade de metros: ");
+//        met = metro.nextFloat();
+//        cm = met * (100f);
+//        mm = cm * (10f);
+//        System.out.println(met +"metros equivale a:"+cm+" cm, e: "+mm +" mm");
+        
+        //exercício 8
+//        Scanner reais = new Scanner(System.in);
+//        float rea,dolar,cota;
+//        System.out.println("Digite o Valor em reais: ");
+//        rea = reais.nextFloat();
+//        System.out.println("Digite a cotação do Dolar atual: ");
+//        cota = reais.nextFloat();
+//        dolar = rea / cota;
+//        System.out.println("O valor em dolar que é possivel comprar é: $"+dolar);
+        
+        //exercício 9
+//        Scanner tamn = new Scanner(System.in);
+//        float alt,lar,area,lit;
+//        System.out.println("Digite a largura da parede: ");
+//        lar = tamn.nextFloat();
+//        System.out.println("Digite a altura da parede: ");
+//        alt = tamn.nextFloat();
+//        area = alt*lar;
+//        lit = (area*0.1f)/2;
+//        System.out.println("A área a ser pintadade é de: "+area+"m2");
+//        System.out.println("A quantidade de litros de tinta a ser utilizada é de: "+lit+"L");
+
+        //exercício 10
+//        Scanner fig = new Scanner(System.in);
+//        float larg,alt,per,area,raio,tri,trialt;
+//        
+//        System.out.println("Digite o lado do quadrado: ");
+//        larg = fig.nextFloat();
+//        per = larg*4;
+//        area = larg*larg;
+//        System.out.println("O perimetro do quadrado é: "+per+" \nA área do quadrado é de: "+area+" m2");
+//        
+//        System.out.println("");
+//        System.out.println("-------------------------------------------------------------------------");
+//        System.out.println("");
+//        
+//        System.out.println("Digite a largura do retangulo: ");
+//        larg = fig.nextFloat();
+//        System.out.println("Digite a altura do retangulo: ");
+//        alt = fig.nextFloat();
+//        per = (larg*2)+(alt*2);
+//        area = larg*alt;
+//        System.out.println("O perimetro do retangulo é: "+per+" \nA área do retangulo é de: "+area+" m2");
+//        
+//        System.out.println("");
+//        System.out.println("-------------------------------------------------------------------------");
+//        System.out.println("");
+//        
+//        System.out.println("Digite o raio do circulo: ");
+//        raio = fig.nextFloat();
+//        per = (2*3.14f)*raio;
+//        area = 3.14f*(raio*raio);
+//        System.out.println("A circunferência do circulo é: "+per+" \nA área do circulo é de: "+area+" m2");
+//        
+//        System.out.println("");
+//        System.out.println("-------------------------------------------------------------------------");
+//        System.out.println("");
+//        
+//        System.out.println("Digite a medida do lado do triangulo: ");
+//        tri = fig.nextFloat();
+//        per = tri*3;
+//        System.out.println("Informe a medida da altura do triangulo: ");
+//        trialt = fig.nextFloat();
+//        area = (tri*trialt)/2;
+//        System.out.println("O perimetro do triangulo é: "+per+" \nA área do triangulo é de: "+area+" m2");
+        
+        
+    
+
+        
+         
+
+   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
